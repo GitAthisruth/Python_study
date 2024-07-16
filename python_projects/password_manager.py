a77f82f49341a68307dcc41531fc267d3b3ef90d
Athisruth <athisruthmsatheesh98@gmail.com>
2024-07-16 15:54:20 +0530

    "added"

--- python_projects/password_manager.py
+++ python_projects/password_manager.py
@@ -1,19 +1,47 @@
-pwd = input("What is the master password? ")
+from cryptography.fernet import Fernet
+
+
+# b"hello"#byte string
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key","wb") as key_file:
+        key_file.write(key)'''
+
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key","wb") as key_file:
+        key_file.write(key)
+
+write_key()
+
+def load_key():
+    with open("key.key", "rb") as key_file:
+        key = key_file.read()
+    return key
+
+
+key = load_key() 
+fer = Fernet(key)
+
 
 
 def view():
-    print("Tim")
-    print("Tim")
+    with open("passwords.txt",'r') as f:
+        for line in f.readlines():
+            # print(line.rstrip())
+            data = line.rstrip()
+            user,passw = data.split("|")
+            print(f"User:{user} Password:{passw}",
+                  fer.decrypt(passw.encode()).decode())
 
-
-view()
-view()
 
 def add():
     name = input("Account Name: ")
     pwd = input("Password: ")
     with open("passwords.txt",'a') as f:
-        f.write(name+"|"+pwd)
+        f.write(name+"|"+fer.encrypt(pwd.encode()).decode()+"\n")
 
 
 while True:
@@ -24,9 +52,11 @@ while True:
 
     elif mode == "view":
         view()
+    
 
     elif mode == "add":
         add()
+        
 
     else:
         print("Invalid mode.")
