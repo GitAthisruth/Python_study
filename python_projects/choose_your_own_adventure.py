37a097fdfca6656757f4d77c7f10e9439f0b543a
Athisruth <athisruthmsatheesh98@gmail.com>
2024-07-16 14:28:34 +0530

    "added"

---
+++ python_projects/choose_your_own_adventure.py
@@ -0,0 +1,39 @@
+# name = input("Type your name: ")
+# print("Welcome",name,"to this adventure!")
+
+# while True:
+#     answer = input(
+#         "You are on a dirt road, it has come to an end and you can go left or right. Which way would you like to go?"
+#     ).lower()
+
+#     if answer == "left":
+#         answer = input("You come to a river, you can walk around it or swim accross? Type 'walk' to walk around and 'swim' to swim accross: ").lower()
+#         if answer == "swim":
+#             print("You swam accross the river and were eaten by an anaconda,you lose the game!!!")
+#             exit() #/break this is a loop so we can use break
+#         elif answer == "walk":
+#             print("You walked for many miles,ran out of water and you lost the game!!!!!")
+#             exit()
+#         else:
+#             print()
+#     elif answer == "right":
+#         print("You come to a bridge, it looks wobbly, do you want to cross it or head back?")
+#         if answer == "back":
+#             print("You go back and lose. ")#we use break is used to control the flow of a loop
+#             exit()
+#         elif answer =="cross":
+#             answer = input("You cross the bridge and meet a stranger.Do you want to talk to them or not?")
+#             if answer == "yes":
+#                 print("You talk to the stranger and they give you gold. You WIN!")
+#             elif answer == "no":
+#                 print("You ignore the stranger and they are offended and you lose.")
+#                 exit()
+#             else:
+#                 print("Not a valid option. You lose.")
+
+#     else:
+#         print("Must choose 'left' or 'right")
+
+
+
+
