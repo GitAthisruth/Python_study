b179d581fe3f6927e9e5883123e515f67f3dff50
Athisruth <athisruthmsatheesh98@gmail.com>
2024-07-24 14:57:16 +0530

    "alarm"

--- python_projects/alarm_clock/alarm_clock.py
+++ python_projects/alarm_clock/alarm_clock.py
@@ -2,9 +2,13 @@
 from playsound import playsound
 import time
 
+CLEAR = "\033[2J"
+CLEAR_AND_RETURN = "\033[H"
+
 def alarm(seconds):
     time_elapsed = 0
 
+    print(CLEAR)
     while time_elapsed < seconds:
         time.sleep(1)
         time_elapsed += 1
@@ -12,9 +16,14 @@ def alarm(seconds):
         minutes_left = time_left // 60 #125 // 60 = 2 ,here 60 evenly divides 125 2 times.
         seconds_left = time_left % 60
 
-        print(f"{minutes_left:02d}:{seconds_left:02d}")#02d addin decimal points
+        print(f"{CLEAR_AND_RETURN}{minutes_left:02d}:{seconds_left:02d}")#02d addin decimal points
+    playsound("alarm-1-with-reverberation-30031.mp3")
 
-alarm(10)
+minutes = int(input("How many minutes to wait: "))
+seconds = int(input("How many seconds to wait: "))
+total_seconds = minutes * 60 + seconds
+
+alarm(total_seconds)
 
 
 
