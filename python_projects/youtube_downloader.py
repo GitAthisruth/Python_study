3e133ca7ec726b09c0f367a73931e95de9f6edaf
Athisruth <athisruthmsatheesh98@gmail.com>
2024-07-26 20:53:48 +0530

    "added"

--- python_projects/youtube_downloader.py
+++ python_projects/youtube_downloader.py
@@ -1,17 +1,39 @@
-from pytube import YouTube
-import tkinter as tk
-from tkinter import filedialog
+# from pytube import YouTube
+# import tkinter as tk
+# from tkinter import filedialog
 
-def download_video(url,save_path):
-    try:
-        yt = Youtube(url)
-        streams = yt.streams.filter(progressive=True,file_extension = "mp4")
-        highest_res_stream = streams.get_highest_resolution()
-        highest_res_stream.download(output_path=save_path)
-        print("Video downloaded successfully")
-    except Exception as e:
-        print(e)
+# def download_video(url, save_path):
+#     try:
+#         yt = YouTube(url)
+#         streams = yt.streams.filter(progressive=True, file_extension="mp4")
+#         highest_res_stream = streams.get_highest_resolution()
+#         highest_res_stream.download(output_path=save_path)
+#         print("Video downloaded successfully!")
+#     except Exception as e:
+#         print(e)
 
-url = "https://www.youtube.com/watch?v=ydOZ5YAv8Hk"
-save_path = "D:\Data Science\Python_study\python_projects"
-download_video(url,save_path)
+# def open_file_dialog():
+#     folder = filedialog.askdirectory()
+#     if folder:
+#         print(f"Selected folder: {folder}")
+#     return folder
+
+
+
+# if __name__ == "__main__":
+#     root = tk.Tk()
+#     root.withdraw()
+#     video_url = input("Please enter a Youtube url: ")
+#     save_dir = open_file_dialog()
+
+#     if save_dir:
+#         print("Started download...")
+#         download_video(video_url,save_dir)
+#     else:
+#         print("Invalid save location.")
+
+
+# # Correctly format the URL without additional query parameters
+# # url = "https://www.youtube.com/watch?v=m9coOXt5nuw"
+# # save_path = "D://Data Science//Python_study//python_projects"
+# # download_video(url, save_path)
