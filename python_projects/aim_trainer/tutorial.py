fc4b8e291b5ef371a45b3ae87100cc76205410c1
Athisruth <athisruthmsatheesh98@gmail.com>
2024-07-27 21:05:57 +0530

    "added"

--- python_projects/aim_trainer/tutorial.py
+++ python_projects/aim_trainer/tutorial.py
@@ -7,8 +7,15 @@ pygame.init()
 
 WIDTH,HEIGHT = 800, 600
 
-WIN = pygame.display.set_mode(WIDTH,HEIGHT)
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Aim Trainer")
+
+TARGET_INCREMENT = 400
+TARGET_EVENT = pygame.USEREVENT
+
+TARGET_PADDING = 30
+
+BG_COLOR = (0,25,40)
 
 class Target:
     MAX_SIZE = 30
@@ -38,16 +45,66 @@ class Target:
         pygame.draw.circle(win,self.COLOR,(self.x,self.y),self.size*0.6)
         pygame.draw.circle(win,self.SECOND_COLOR,(self.x,self.y),self.size*0.4)
 
+    def collide(self,x,y):
+        dis = math.sqrt((self.x - x)**2 + (self.y - y)**2)
+        return dis <= self.size
+
+
+def draw(win,targets):
+    win.fill(BG_COLOR)
+
+    for target in targets:
+        target.draw(win)
+
+    pygame.display.update()
 
 def main():
     run = True
     targets =  []
-    
+    clock = pygame.time.Clock()
+
+    target_pressed = 0
+    clicks = 0
+    misses = 0
+    start_time = time.time()
+
+    pygame.time.set_timer(TARGET_EVENT,TARGET_INCREMENT)
+
     while run:
+        clock.tick(60)
+        click = False
+        mouse_pos = pygame.mouse.get_pos()
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+            if event.type == TARGET_EVENT:
+                x = random.randint(TARGET_PADDING,WIDTH)
+                y = random.randint(TARGET_PADDING,HEIGHT-TARGET_PADDING)
+                target = Target(x,y)
+                targets.append(target)
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = True
+                clicks += 1
+
+        for target in targets:
+            target.update()
+
+            if target.size <= 0:
+                targets.remove(target)
+                misses += 1
+
+            if click and target.collide(*mouse_pos):#*mouse_pos -astrick used to break down the tuple into individual elements.
+                targets.remove(target)
+                target_pressed += 1
+
+
+        draw(WIN,targets)
+
     pygame.quit()
 
 if __name__ == "__main__":
